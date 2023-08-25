874600e562140aadacf7fe57c0031f5bac7460f6
Onthada Preedasawakul <rrrealrs@gmail.com>
2023-08-25 16:31:18 +0700

    initial commit

---
+++ R/D10_data.R
@@ -0,0 +1,12 @@
+#' @title D10 Artificial Dataset
+#' @description A \code{2}-dimensional dataset generated from \code{3} different Gaussian and \code{2} Uniform distributions labeled as \code{1-5}.
+#' @format A data frame with 1250 data points and 3 variables
+#' \describe{
+#'   \item{\code{x}}{Numeric values generated from Gaussian and Uniform distributions}
+#'   \item{\code{y}}{Numeric values generated from Gaussian and Uniform distributions}
+#'   \item{\code{label}}{Categorical labels {1,2,3,4,5}}
+#'}
+#' @references  {xxx}
+#' @author {Nathakhun Wiroonsri and Onthada Preedasawakul}
+#' @seealso {\link{FzzyCVIs},  \link{WP.IDX},  \link{R1_data}}
+"D10_data"
---
+++ R/D5_data.R
@@ -0,0 +1,12 @@
+#' @title D5 Artificial Dataset
+#' @description A \code{2}-dimensional dataset generated from \code{5} different Gaussian distributions labeled as \code{1-5}.
+#' @format A \code{data frame} with 350 data points and 3 variables
+#' \describe{
+#'   \item{\code{x}}{Numeric values generated from Gaussian distributions}
+#'   \item{\code{y}}{Numeric values generated from Gaussian distributions}
+#'   \item{\code{label}}{Categorical labels {1,2,3,4,5}}
+#'}
+#' @references  {xxx}
+#' @author {Nathakhun Wiroonsri and Onthada Preedasawakul}
+#' @seealso {\link{FzzyCVIs},  \link{WP.IDX},  \link{R1_data}}
+"D5_data"
